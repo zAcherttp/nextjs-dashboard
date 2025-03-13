0ba1e36638672ec873177ba5fb3db6afb097b6a7
Thang Tuấn Phát <Saladegg24@gmail.com>
2025-03-13 19:32:00 +0700

    linting

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -15,10 +15,7 @@ import { useSearchParams } from "next/navigation";
 export default function LoginForm() {
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get("callbackUrl") || "/dashboard";
-  const [errorMessage, formAction, isPending] = useActionState(
-    authenticate,
-    undefined
-  );
+  const [errorMessage, formAction] = useActionState(authenticate, undefined);
 
   return (
     <form action={formAction} className="space-y-3">
